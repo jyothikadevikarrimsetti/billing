d9c335a6a7e0bc6cac97811df26fc6a82acece6a
JimsonR <jimsonjimmy2008@gmail.com>
2025-02-10 13:56:55 +0530

    set up cors

--- billingfront/src/environments/environment.prod.ts
+++ billingfront/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production:true,
-    apiUrl: 'http://ec2-3-111-37-183.ap-south-1.compute.amazonaws.com'
+    apiUrl: 'http://ec2-13-201-137-53.ap-south-1.compute.amazonaws.com'
 }
