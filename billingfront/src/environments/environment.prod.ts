93dd2f727e4c4cd5d35c2bd50f4e9f6f0f7e9552
JimsonR <jimsonjimmy2008@gmail.com>
2025-02-10 23:06:54 +0530

    cors fix and ssl fix

--- billingfront/src/environments/environment.prod.ts
+++ billingfront/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production:true,
-    apiUrl: 'http://ec2-13-203-103-42.ap-south-1.compute.amazonaws.com/api'
+    apiUrl: 'https://ec2-13-203-103-42.ap-south-1.compute.amazonaws.com/api'
 }
--- billingsystem/src/main/java/com/example/billingsystem/config/SecurityConfig.java
+++ billingsystem/src/main/java/com/example/billingsystem/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
         // corsConfiguration.addAllowedOriginPattern("*");
         // corsConfiguration.addAllowedOrigin("http://localhost");
 //        corsConfiguration.addAllowedOrigin("http://localhost:4200");
-        corsConfiguration.addAllowedOrigin("http://ec2-13-203-103-42.ap-south-1.compute.amazonaws.com");
+        corsConfiguration.addAllowedOrigin("https://ec2-13-203-103-42.ap-south-1.compute.amazonaws.com");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
 
