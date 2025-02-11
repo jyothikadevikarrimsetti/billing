68fb79d16cca066eb683315393436c53916c26a6
JimsonR <jimsonjimmy2008@gmail.com>
2025-02-11 08:20:57 +0530

    cors ad ssl

--- billingfront/src/environments/environment.prod.ts
+++ billingfront/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production:true,
-    apiUrl: 'https://ec2-65-2-74-85.ap-south-1.compute.amazonaws.com'
+    apiUrl: 'https://ec2-65-2-74-85.ap-south-1.compute.amazonaws.com/api'
 }
